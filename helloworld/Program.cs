ad7dc4e88c339e56a108eeb1349d56e37b23d1f1
Saikumar <muthyalasaikumar9@gmail.com>
2022-04-28 16:56:13 +0530

    first commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace helloworld
+{
+    class program
+    {
+        static void Main (String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void printname()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+}
